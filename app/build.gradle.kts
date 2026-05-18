874b685a6aaa00e916ee9401d2e400cf55a5c4d5
DarkBladeDev <darkbladedev@gmail.com>
2026-05-17 23:02:37 -0300

    fix: resolve JitPack build issues (Java 21 and maven-publish plugin)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,15 @@
 
 plugins {
     `java-library`
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
 
 repositories {
